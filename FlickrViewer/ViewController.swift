2164040e7aac411d9a3c0fa32d1498cd3d5fe4e2
yjiq150 <yjiq150@gmail.com>
2017-01-07 17:19:48 +0900

    Initial Commit

---
+++ FlickrViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrViewer
+//
+//  Created by yjiq150 on 07/01/2017.
+//  Copyright © 2017 yjiq150. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
